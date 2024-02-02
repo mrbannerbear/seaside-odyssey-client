463344dc16c859c0d643ce68e6effd2adb5dec4b
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-02-02 19:49:27 +0600

    Minor update

--- src/app/components/homeComps/Features.js
+++ src/app/components/homeComps/Features.js
@@ -16,7 +16,7 @@ const Features = () => {
         <h2 className="text-3xl text-red-900 py-2 text-center pb-4">Included in Our Cruises</h2>
         <div className=" flex flex-wrap justify-center gap-5">
         {features.map((each) => (
-        <div className="w-44 bg-red-50 rounded overflow-hidden shadow-lg text-center h-28 hover:transition-all hover:scale-105">
+        <div className="w-44 bg-red-50 rounded overflow-hidden shadow-lg text-center h-28">
           {/* <img className="w-full" src={each.logo} alt={each.title} /> */}
           <div className="px-6 py-4">
             <div className="text-red-900 text-xl mb-2 hover:cursor-pointer hover:border-b-[1.5px] pb-1">{each.title}</div>
