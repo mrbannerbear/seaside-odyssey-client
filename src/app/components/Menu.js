ea0fd5ae0fd6c87bcd18a5e9515229de8a619e7b
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-02-02 18:30:55 +0600

    Minor update

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -2,8 +2,9 @@ export default function Menu() {
   return (
     <div className="py-6">
     <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-red-50">Home</a>
-    <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-red-50">About</a>
-    <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-red-50">Services</a>
+    <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-red-50">About Us</a>
+    <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-red-50">Destinations</a>
+    <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-red-50">Ships</a>
     <a href="#" className="block px-4 py-2 text-gray-800 hover:bg-red-50">Contact</a>
   </div>
   )
